7a9f3e764d1e8484cd135dcab4f0b112a3300e42
Tiemoyo <57482850+Tiemoyo@users.noreply.github.com>
2020-01-09 22:00:52 +0100

    Add files via upload

---
+++ zuul-better/zuul-better/Maingame.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class MainGame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Maingame
+{
+   public static void main (String[] args)
+   {
+   Game var = new Game();
+   var.play();
+}
+}
